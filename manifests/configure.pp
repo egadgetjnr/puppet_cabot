755430bd50d010927db89f8a14a31bf11168dc29
Lavaburn <nicolas@truyens.com>
2015-04-23 14:49:20 +0200

    Bugfixes

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -128,8 +128,8 @@ class cabot::configure inherits ::cabot {
   exec { 'cabot init-script':
     command     => "bash -c 'export HOME=$source_dir; ${foreman} export upstart /etc/init -f ${procfile} -e ${env_file} -u ${user} -a cabot -t ${template}'",
     cwd         => $source_dir,
-    #subscribe   => Exec['cabot compress'],
-    #refreshonly => true,
+    subscribe   => Exec['cabot compress'],
+    refreshonly => true,
     path        => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
   }
 
