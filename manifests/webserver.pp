551f4808be7b62886bfea1430d6ffe666a34f23b
Lavaburn <nicolas@truyens.com>
2015-04-23 16:10:48 +0200

    Proxy sequence important?

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -39,15 +39,12 @@ class cabot::webserver inherits ::cabot {
 	    proxy_preserve_host => true,
       proxy_pass          => [
         {
-          'path' => '/',
-          'url'   => 'http://127.0.0.1:5000/',
-#       proxy_set_header Host \$http_host;
-#       proxy_set_header X-Real-IP \$remote_addr;
-#       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        },
-        {
           'path' => '/static',
           'url'   => '!',
+        },
+        {
+          'path' => '/',
+          'url'   => 'http://127.0.0.1:5000/',
         },
       ],
 	  }
