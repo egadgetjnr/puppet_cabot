e2e9cc2e9e0cde4fc066d1d3e4785fc5f9c7a959
Lavaburn <nicolas@truyens.com>
2015-04-23 15:56:11 +0200

    Proxy path, not proxy

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -39,14 +39,14 @@ class cabot::webserver inherits ::cabot {
 	    proxy_preserve_host => true,
       proxy_pass          => [
         {
-          'proxy' => '/',
+          'path' => '/',
           'url'   => 'http://127.0.0.1:5000/',
 #       proxy_set_header Host \$http_host;
 #       proxy_set_header X-Real-IP \$remote_addr;
 #       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         },
         {
-          'proxy' => '/static',
+          'path' => '/static',
           'url'   => '!',
         },
       ],
